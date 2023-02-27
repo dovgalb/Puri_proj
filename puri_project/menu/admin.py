1dfc3446107b4a39eefe7b8d0e7ebc001c373b44
dovgalb <bogdandovgal1208@gmail.com>
2023-02-27 22:24:48 +0300

    исправил верстку состава запраки, добавил отображение subcategory_id, category_id в админке

--- puri_project/menu/admin.py
+++ puri_project/menu/admin.py
@@ -7,20 +7,25 @@ from .models import *
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ['name', 'subcategory', 'create_date', 'update_date', ]
     filter_horizontal = ['compound', 'sauce']
-    search_fields = ['name__iregex','compound__name__iregex', 'sauce__name__iregex' ]
+    search_fields = ['name__iregex', 'compound__name__iregex', 'sauce__name__iregex' ]
+    list_filter = ['category', 'subcategory']
+
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category' ]
+    list_display = ['id', 'name', 'category' ]
+    list_display_links = ['id', 'name',]
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', ]
+    list_display = ['id', 'name', ]
+    list_display_links = ['id', 'name', ]
+
 
 
 @admin.register(Compound)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', ]
     filter_horizontal = ['compound']
-    search_fields   = ['name__iregex','compound__name__iregex', ]
+    search_fields   = ['name__iregex', 'compound__name__iregex', ]
